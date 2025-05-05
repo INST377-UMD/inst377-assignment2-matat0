ca869deda3950c7105987a41b800f4a3706bba62
matat0 <mattbangit@gmail.com>
2025-05-04 23:55:46 -0400

    dog

--- dogs_script.js
+++ dogs_script.js
@@ -1,16 +1,33 @@
+window.onload = main;
 
+async function main() {
+  try {
+    const response = await fetch(`https://dog.ceo/api/breeds/image/random/10`);
+    const result = await response.json();
+    console.log(result);
 
+    const container = document.getElementById("slider");
 
+    for (let i = 0; i < result.message.length;i++) {
+      const img = document.createElement('img');
+      img.src = result.message[i];
+      container.appendChild(img);
+    }
 
+    simpleslider.getSlider();
+  }
 
+  catch(err) {
+    console.log(err);
+    return err.message;
+  }
 
+  finally {
+    console.log("finally");
+  }
 
-
-
-
-
-
-
+  simpleslider.getSlider();
+}
 
 function enableListening() {
     if (annyang) {
